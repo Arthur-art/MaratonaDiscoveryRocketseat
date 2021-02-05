ea92cef9bf0a8a49efece0133552262e3d1e0672
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-05 17:45:10 -0300

    atualizando devfinance modal

---
+++ scripts.js
@@ -0,0 +1,42 @@
+/*Adicionar ou remover classe active na classe modal-overlay parar abrir ou fechar o Modal */
+const Modal = {
+  open() {
+    document.querySelector(".modal-overlay").classList.add("active");
+  },
+  close() {
+    document.querySelector(".modal-overlay").classList.remove("active");
+  },
+};
+
+const transactions = [
+  {
+    id: 1,
+    description: "Luz",
+    amount: -50000,
+    date: "23/01/2021",
+  },
+  {
+    id: 2,
+    description: "Criação website",
+    amount: 500000,
+    date: "23/01/2021",
+  },
+  {
+    id: 3,
+    description: "Internet",
+    amount: -20000,
+    date: "23/01/2021",
+  },
+];
+
+const Transaction = {
+  incomes() {
+    //somar as entradas
+  },
+  expenses() {
+    //somar as saidas
+  },
+  total() {
+    //entradas - saidas
+  },
+};
