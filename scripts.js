03af146fbf9f691dd07ba38eefea64f187b692b6
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-07 19:14:18 -0300

    adicionando js a aplicacao

--- scripts.js
+++ scripts.js
@@ -27,6 +27,12 @@ const transactions = [
     amount: -20000,
     date: "23/01/2021",
   },
+  {
+    id: 4,
+    description: "App",
+    amount: 20000,
+    date: "23/01/2021",
+  },
 ];
 
 const Transaction = {
@@ -40,3 +46,51 @@ const Transaction = {
     //entradas - saidas
   },
 };
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index){
+
+    var amount  = Utils.formatCurrency(transaction.amount);
+    
+    const tr = document.createElement('tr');
+    tr.innerHTML =DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+
+    //Trocando a classe 
+    const TransactionAmount = transaction.amount;
+    var CSSclass = [];
+    if(TransactionAmount>0){
+      CSSclass = ["income"];
+    }else{
+      CSSclass = ["expense"]
+    }
+
+    const html = `
+
+    
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${transaction.amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td><img src="./assets/minus.svg" alt="" /></td>
+    
+      
+      `
+      return html
+  }
+};
+
+const Utils = {
+  formatCurrency(value){
+    const signal = Number(value) < 0 ? "-" : ""
+  }
+}
+
+transactions.forEach(function(transaction){
+  DOM.addTransaction(transaction);
+})
+
