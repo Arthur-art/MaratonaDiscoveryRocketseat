9cf5ec05b229d2a21fb9f9e6da165366bbdb37a9
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-11 21:01:58 -0300

    Aplication Discovery devFinance

--- scripts.js
+++ scripts.js
@@ -8,43 +8,26 @@ const Modal = {
   },
 };
 
-const Transaction = {
-  
-  all: [
-    {
-      
-      description: "Luz",
-      amount: -50000,
-      date: "23/01/2021",
-    },
-    {
-      
-      description: "Criação website",
-      amount: 500000,
-      date: "23/01/2021",
-    },
-    {
-      
-      description: "Internet",
-      amount: -20000,
-      date: "23/01/2021",
-    },
-    {
-      
-      description: "App",
-      amount: 20000,
-      date: "23/01/2021",
-    },
-  ],
+const Storage = {
+  get(){
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
+  },
+  set(transactions){
+      localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+  }
+}
 
-  add(transaction){
+const Transaction = {
+  all: Storage.get(),
+
+  add(transaction) {
     Transaction.all.push(transaction);
-    App.reload()
+    App.reload();
   },
 
-  remove(index){
-    Transaction.all.splice(index, 1)
-    App.reload()
+  remove(index) {
+    Transaction.all.splice(index, 1);
+    App.reload();
   },
 
   incomes() {
@@ -64,7 +47,7 @@ const Transaction = {
     let expense = 0;
     //Pegar todas as transacoes
     //para cada transacao, se ela for menor que zero
-   Transaction.all.forEach(function (transaction) {
+    Transaction.all.forEach(function (transaction) {
       if (transaction.amount < 0) {
         expense += transaction.amount;
       }
@@ -81,14 +64,16 @@ const DOM = {
   //Adicionando o conteudo de innerHTMLTransaction na dom dentro do elemento table
   transactionsContainer: document.querySelector("#data-table tbody"),
 
+  //o index é o numero da posicao do array que o objeto estara guardado
   addTransaction(transaction, index) {
     const tr = document.createElement("tr");
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
 
     DOM.transactionsContainer.appendChild(tr);
   },
 
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     //Trocando a classe do element td para mudar a cor dos numero negativos e positivos
     const TransactionAmount = transaction.amount;
     var CSSclass = [];
@@ -106,7 +91,7 @@ const DOM = {
       <td class="description">${transaction.description}</td>
       <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
-      <td><img src="./assets/minus.svg" alt="" /></td>
+      <td><img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="" /></td>
     
       
       `;
@@ -125,13 +110,25 @@ const DOM = {
       Transaction.total()
     );
   },
-  clearTransactions(){
+  clearTransactions() {
     DOM.transactionsContainer.innerHTML = "";
-  }
+  },
 };
 
 //Classe com metodos para formatar os valores amount na DOM
 const Utils = {
+  formatAmount(value) {
+    //retirando virgulas e pontos e multiplicando por 100
+    value = Number(value.replace(/\,\./g, "")) * 100;
+
+    return value;
+  },
+
+  formatDate(date) {
+    const splittedDate = date.split("-");
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
+
   formatCurrency(value) {
     const signal = Number(value) < 0 ? "-" : "";
     value = String(value).replace(/\D/g, "");
@@ -145,36 +142,95 @@ const Utils = {
 };
 
 const Form = {
-    submit(event){
-      //interrompe um compartamento padrão para que eu faça as funcionalidade que eu quiser
-     event.preventDefault()
+  description: document.querySelector("input#description"),
+  amount: document.querySelector("input#amount"),
+  date: document.querySelector("input#date"),
 
-     //verificar se todas as informações foram preenchidas
-     //formatar os dados para salvar
-     //salvar
-     //apagar os dados do formulario
-     //fechar o modal
-     //Atualizar a aplicacao
-    }
-}
-
-const App = {
-  init(){
-    //Para cada objeto dentro do array, utlizar a função DOM.addTransaction(transaction)
-Transaction.all.forEach(function (transaction) {
-  DOM.addTransaction(transaction);
-});
-
-DOM.updateBalance();
-
-
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
   },
-  reload(){
-    DOM.clearTransactions();
-    App.init()
+
+  formatData() {
+    //console.log("formatar os dados")
+  },
+
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
+
+    //verificando se o valor é vazio e gerar um erro se estiver vazio
+    if (
+      description.trim() === "" ||
+      amount.trim() === "" ||
+      date.trim() === ""
+    ) {
+      throw new Error("Por favor, preencha todos os campos");
+    }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+
+    amount = Utils.formatAmount(amount);
+
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date,
+    };
+  },
+
+  clearFields() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+  },
+
+  submit(event) {
+    //interrompe um compartamento padrão para que eu faça as funcionalidade que eu quiser
+    event.preventDefault();
+    try {
+      //varificar se os campos sao validos
+      Form.validateFields();
+      const transaction = Form.formatValues();
+
+      //input no array de objetos
+      Transaction.add(transaction);
+
+      //apagar os dados do formulario
+      Form.clearFields();
+
+      //fechar o modal
+      Modal.close();
+    } catch (error) {
+      alert(error.message);
+    }
+
+    //formatar os dados para salvar
+    Form.formatData();
   },
 };
 
-App.init()
+const App = {
+  init() {
+    //Para cada objeto dentro do array, utlizar a função DOM.addTransaction(transaction)
+    Transaction.all.forEach(function (transaction, index) {
+      DOM.addTransaction(transaction, index);
+    });
 
+    Storage.set(Transaction.all)
 
+    DOM.updateBalance();
+  },
+  reload() {
+    DOM.clearTransactions();
+    App.init();
+  },
+};
+
+App.init();
